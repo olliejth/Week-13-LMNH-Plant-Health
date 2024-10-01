df4c553c6c0e571961bcf8f97aa1e4bc6bbd2c67
Leon <leonjs02@protonmail.com>
2024-10-01 16:29:47 +0100

    Removed reset script

--- long_term/reset.sh
+++
@@ -1,4 +0,0 @@
-source .env
-psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "DROP DATABASE IF EXISTS $DB_NAME;"
-psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "CREATE DATABASE $DB_NAME;"
-psql -h $DB_HOST -p $DB_PORT -U $DB_USER -f schema.sql

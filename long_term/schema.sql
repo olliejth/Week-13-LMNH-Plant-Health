80ebbdfec67669c8bc50dabb51793b06d0ceaf2b
Leon <leonjs02@protonmail.com>
2024-10-01 11:48:18 +0100

    Added two table schemas and renamed readme.

--- long_term/schema.sql
+++ long_term/schema.sql
@@ -1,1 +1,25 @@
 -- SQL to initialise database.
+
+DROP TABLE IF EXISTS reading;
+DROP TABLE IF EXISTS plant;
+DROP TABLE IF EXISTS origin_location;
+DROP TABLE IF EXISTS botanist;
+
+CREATE TABLE botanist(
+    botanist_id SMALLINT GENERATED ALWAYS AS IDENTITY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(30) NOT NULL,
+    PRIMARY KEY botanist_id
+);
+
+CREATE TABLE origin_location(
+    location_id SMALLINT GENERATED ALWAYS AS IDENTITY,
+    latitude DECIMAL(11,8) NOT NULL,
+    longitude DECIMAL(11,8) NOT NULL,
+    town VARCHAR(255) NOT NULL,
+    timezone VARCHAR(255) NOT NULL,
+    PRIMARY KEY location_id
+);
+

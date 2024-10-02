01cbf63955a15bf53815dc645c7606beebff88bc
Leon <leonjs02@protonmail.com>
2024-10-02 10:21:21 +0100

    Adding functions to recreate pipeline.

--- short_term/database_handler.py
+++ short_term/database_handler.py
@@ -5,3 +5,15 @@ from os import environ as ENV
 
 from pymssql import connect
 from dotenv import load_dotenv
+
+
+def get_connection(dict_handler: bool = True):
+    """Returns a database connection."""
+    return connect(
+        server=ENV['DB_HOST'],
+        port=ENV['DB_PORT'],
+        user=ENV['DB_USER'],
+        password=ENV['DB_PASSWORD'],
+        database=ENV['DB_NAME'],
+        as_dict=dict_handler
+    )

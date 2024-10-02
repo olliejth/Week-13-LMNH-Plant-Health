7e6d3505448cb2386aa6dfd5751057c073e31443
Leon <leonjs02@protonmail.com>
2024-10-02 11:03:26 +0100

    Added function to insert the readings data.

--- short_term/load.py
+++ short_term/load.py
@@ -1,2 +1,15 @@
 """Given a file name uploads local json file to S3 bucket."""
 from os import environ as ENV
+
+from database_handler import get_connection
+
+
+def upload_recordings(data: list[tuple]):
+    """Uploads the reading/recording information to the database."""
+    insert_query = """INSERT INTO reading(plant_id, botanist_id, 
+    at, soil_moisture, temperature, last_watered)
+    VALUES %s"""
+    with get_connection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(insert_query, data)
+        conn.commit()

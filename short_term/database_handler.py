539bb80565d7abd12051ab3686e81d3a2ab369d2
Leon <leonjs02@protonmail.com>
2024-10-02 10:58:23 +0100

    Removed unnecessary param.

--- short_term/database_handler.py
+++ short_term/database_handler.py
@@ -6,8 +6,10 @@ from os import environ as ENV
 from pymssql import connect
 from dotenv import load_dotenv
 
+load_dotenv()
 
-def get_connection(dict_handler: bool = True):
+
+def get_connection():
     """Returns a database connection."""
     return connect(
         server=ENV['DB_HOST'],
@@ -15,5 +17,23 @@ def get_connection(dict_handler: bool = True):
         user=ENV['DB_USER'],
         password=ENV['DB_PASSWORD'],
         database=ENV['DB_NAME'],
-        as_dict=dict_handler
+        as_dict=True
     )
+
+
+def get_botanist_ids() -> dict:
+    """Returns a dictionary mapping botanist names to IDs."""
+    query_str = """
+        SELECT botanist_id, first_name, last_name 
+        FROM botanist;
+    """
+    with get_connection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(query_str)
+            result = cursor.fetchall()
+
+    botanist_dict = {
+        f"{row['first_name']} {row['last_name']}": row['botanist_id']
+        for row in result
+    }
+    return botanist_dict

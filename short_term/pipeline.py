5d935b01c6f4def5f571e1dd9e5b7f8376f1be41
Leon <leonjs02@protonmail.com>
2024-10-02 13:40:05 +0100

    removed main.

--- short_term/pipeline.py
+++ short_term/pipeline.py
@@ -2,15 +2,14 @@
 
 from extract import extract_recordings
 from transform import transform_recordings
-from load import upload_recordings
+from load import upload_readings
 
 
 def lambda_handler(event=None, context=None):
     """Extracts and uploads API data to DB."""
-
     file_name = extract_recordings()
     transformed_data = transform_recordings(file_name)
-    upload_recordings(transformed_data)
+    upload_readings(transformed_data)
 
     return {
         'statusCode': 200,

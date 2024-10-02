4dd8d7a570c7e41c8ef63633098fc87f3e5a7ebc
Shayak Hussain <shayak201@gmail.com>
2024-10-02 18:54:20 +0100

    fix test case

--- short_term/test_load_short.py
+++ short_term/test_load_short.py
@@ -1,65 +1,64 @@
 # pylint: skip-file
 
-import unittest
 from datetime import datetime
 from unittest.mock import patch, MagicMock
 from load_short import convert_at_datetime, convert_last_watered_datetime, format_reading_tuples, upload_readings
 
 
-class TestLoadShort(unittest.TestCase):
+def test_convert_at_datetime():
+    date_str = "2024-10-01 12:30:45"
+    expected = datetime(2024, 10, 1, 12, 30, 45)
+    result = convert_at_datetime(date_str)
+    assert result == expected
 
-    def test_convert_at_datetime(self):
-        date_str = "2024-10-01 12:30:45"
-        expected = datetime(2024, 10, 1, 12, 30, 45)
-        result = convert_at_datetime(date_str)
-        self.assertEqual(result, expected)
 
-    def test_convert_last_watered_datetime(self):
-        date_str = "Mon, 01 Oct 2024 12:30:45 GMT"
-        expected = datetime(2024, 10, 1, 12, 30, 45)
-        result = convert_last_watered_datetime(date_str)
-        self.assertEqual(result, expected)
+def test_convert_last_watered_datetime():
+    date_str = "Mon, 01 Oct 2024 12:30:45 GMT"
+    expected = datetime(2024, 10, 1, 12, 30, 45)
+    result = convert_last_watered_datetime(date_str)
+    assert result == expected
 
-    def test_format_reading_tuples(self):
-        data = [
-            (1, 101, "2024-10-01 12:30:45", "30.5",
-             "22.5", "Mon, 01 Oct 2024 12:30:45 GMT"),
-            (2, 102, "2024-10-02 11:30:45", "25.0",
-             "21.0", "Sun, 30 Sep 2024 12:30:45 GMT")
-        ]
-        expected = [
-            (1, 101, datetime(2024, 10, 1, 12, 30, 45), "30.5",
-             "22.5", datetime(2024, 10, 1, 12, 30, 45)),
-            (2, 102, datetime(2024, 10, 2, 11, 30, 45), "25.0",
-             "21.0", datetime(2024, 9, 30, 12, 30, 45)),
-        ]
-        result = format_reading_tuples(data)
-        self.assertEqual(result, expected)
 
-    @patch('load_short.get_connection')
-    @patch('load_short.get_plant_ids')
-    def test_upload_readings(self, mock_get_plant_ids, mock_get_connection):
-        mock_get_plant_ids.return_value = [1, 2]
+def test_format_reading_tuples():
+    data = [
+        (1, 101, "2024-10-01 12:30:45", "30.5",
+         "22.5", "Mon, 01 Oct 2024 12:30:45 GMT"),
+        (2, 102, "2024-10-02 11:30:45", "25.0",
+         "21.0", "Sun, 30 Sep 2024 12:30:45 GMT")
+    ]
+    expected = [
+        (1, 101, datetime(2024, 10, 1, 12, 30, 45), "30.5",
+         "22.5", datetime(2024, 10, 1, 12, 30, 45)),
+        (2, 102, datetime(2024, 10, 2, 11, 30, 45), "25.0",
+         "21.0", datetime(2024, 9, 30, 12, 30, 45)),
+    ]
+    result = format_reading_tuples(data)
+    assert result == expected
 
-        mock_connection = MagicMock()
-        mock_cursor = MagicMock()
-        mock_get_connection.return_value.__enter__.return_value = mock_connection
-        mock_connection.cursor.return_value = mock_cursor
 
-        tuples = [
-            (1, 101, "2024-10-01 12:30:45", "30.5",
-             "22.5", "Mon, 01 Oct 2024 12:30:45 GMT"),
-            (2, 102, "2024-10-02 11:30:45", "25.0",
-             "21.0", "Sun, 30 Sep 2024 12:30:45 GMT"),
-            (3, 103, "2024-10-03 10:30:45", "40.0", "24.0",
-             "Sat, 29 Sep 2024 12:30:45 GMT"),
-        ]
+@patch('load_short.get_connection')
+@patch('load_short.get_plant_ids')
+def test_upload_readings(mock_get_plant_ids, mock_get_connection):
+    mock_get_plant_ids.return_value = [1, 2]
 
-        upload_readings(tuples)
+    mock_connection = MagicMock()
+    mock_cursor = MagicMock()
+    mock_get_connection.return_value.__enter__.return_value = mock_connection
+    mock_connection.cursor.return_value = mock_cursor
 
-        self.assertEqual(mock_cursor.execute.call_count, 2)
+    tuples = [
+        (1, 101, "2024-10-01 12:30:45", "30.5",
+         "22.5", "Mon, 01 Oct 2024 12:30:45 GMT"),
+        (2, 102, "2024-10-02 11:30:45", "25.0",
+         "21.0", "Sun, 30 Sep 2024 12:30:45 GMT"),
+        (3, 103, "2024-10-03 10:30:45", "40.0", "24.0",
+         "Sat, 29 Sep 2024 12:30:45 GMT"),
+    ]
 
-        expected_query = """INSERT INTO beta.reading (plant_id, botanist_id,
-        at, soil_moisture, temperature, last_watered) VALUES """
-        self.assertIn(expected_query,
-                      mock_cursor.execute.call_args_list[0][0][0])
+    upload_readings(tuples)
+
+    assert mock_cursor.execute.call_count == 2
+
+    expected_query = """INSERT INTO beta.reading (plant_id, botanist_id,
+    at, soil_moisture, temperature, last_watered) VALUES ( %s, %s, %s, %s, %s, %s )"""
+    assert expected_query in mock_cursor.execute.call_args_list[0][0][0]
--- short_term/test_transform_short.py
+++ short_term/test_transform_short.py
@@ -21,14 +21,16 @@ def test_format_tuple_data_type(mock_botanist_details):
         "plant_id": 1,
         "botanist_name": "Jane Doe",
         "at": "2024-10-01T10:00:00Z",
-        "soil_moisture": "30.5",
-        "temperature": "22.5",
+        "soil_moisture": 30.5,
+        "temperature": 22.5,
         "last_watered": "2024-09-30"
     }
     result = format_tuple(reading_data, mock_botanist_details)
 
     assert isinstance(result, tuple)
-    assert isinstance(result[0], str)
+    assert isinstance(result[0], int)
+    assert isinstance(result[1], str)
+    assert isinstance(result[3], str)
 
 
 def test_format_tuple_with_input(mock_botanist_details):

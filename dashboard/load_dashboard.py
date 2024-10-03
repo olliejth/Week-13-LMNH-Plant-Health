27497a05e10714fe29a2e2cc1097d71a401ff683
olliejth <ojthompson.64@gmail.com>
2024-10-03 10:20:46 +0100

    Tidying up streamlit load file.

--- dashboard/load_dashboard.py
+++ dashboard/load_dashboard.py
@@ -15,21 +15,11 @@ if __name__ == "__main__":
     temp_line_chart = create_temperature_line(plants_df)
     moist_bar_chart = create_moisture_bar(plants_df)
 
-    plant_ids = plants_df["plant_id"].unique().tolist()
     st.sidebar.markdown("# Sidebar")
-    st.sidebar.multiselect("Trucks", options=plant_ids)
+    plant_ids = plants_df["plant_id"].unique().tolist()
+    st.sidebar.multiselect("Plant IDs", options=plant_ids)
 
     st.altair_chart(botanist_plants_pie, use_container_width=True)
     st.altair_chart(temp_bar_chart, use_container_width=True)
     st.altair_chart(temp_line_chart, use_container_width=True)
     st.altair_chart(moist_bar_chart, use_container_width=True)
-
-
-# cols = st.columns(2)
-#    with cols[0]:
-#         st.altair_chart(total_transactions, use_container_width=True)
-#         st.altair_chart(avg_transactions, use_container_width=True)
-
-#    with cols[1]:
-#         st.altair_chart(transactions_over_time, use_container_width=True)
-#         st.altair_chart(weekday_transactions, use_container_width=True)

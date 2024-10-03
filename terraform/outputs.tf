8726de9f23dca7bc463702a7c0be5c7a247320e5
Shayak Hussain <shayak201@gmail.com>
2024-10-03 13:12:41 +0100

    added EC2 instance

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,3 +3,11 @@
 output "etl_short_term_lambda_arn" {
     value = aws_lambda_function.lmnh_etl_short_term_lambda.arn
 }
+
+output "EC2_public_ip" {
+    value = aws_instance.pipeline_ec2.public_ip
+}
+
+output "EC2_public_dns" {
+    value = aws_instance.pipeline_ec2.public_dns
+}

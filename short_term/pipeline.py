051d12f95eaaeb6ab5527e96c6fd75633c6c40bc
mehrdadhalali <trainee.mehrdad.halali@sigmalabs.co.uk>
2024-10-03 10:12:08 +0100

    Removed some unimporvable pylint checks.

--- short_term/pipeline.py
+++ short_term/pipeline.py
@@ -5,7 +5,7 @@ from transform import transform_recordings
 from load import upload_readings
 
 
-def lambda_handler(event=None, context=None):
+def lambda_handler(event=None, context=None):  # pylint: disable=W0613
     """Extracts and uploads API data to DB."""
     file_name = extract_recordings()
     transformed_data = transform_recordings(file_name)

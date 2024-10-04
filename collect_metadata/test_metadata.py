1433b76838cd12657f432eda7c4cc6740d672efb
olliejth <ojthompson.64@gmail.com>
2024-10-04 14:34:49 +0100

    Added tests.

--- collect_metadata/test_metadata.py
+++ collect_metadata/test_metadata.py
@@ -85,7 +85,7 @@ class TestTransform:
 
         input_dict_list = TestTransform.example_data
 
-        keys = [input_dict_list[0].keys()]
+        keys = list(input_dict_list[0].keys())
 
         output = turn_into_csv_input(input_dict_list)
 
